19ddb957d37f454704058409c1454786fb8cb595
William Neve <williamneve6000@gmail.com>
2023-12-21 03:00:45 +0000

    sleepy time

--- src/app/components/terminal/terminal.js
+++ src/app/components/terminal/terminal.js
@@ -1,5 +1,24 @@
+import styles from './terminal.module.scss'
+
+function Line({content, extra}) {
+  return (
+    <p>{content} and {extra}</p>
+  )
+}
+
+function Input() {
+  return (
+    <textarea className={styles.input}>test</textarea>
+  )
+}
+
 export default function Terminal() {
   return (
-    <div>Test</div>
+    <div className={styles.wrapper}>
+      <div className={styles.linesContainer}>
+        <Line content={'test'} extra='hello'/>
+      </div>
+      <Input/>
+    </div>
   )
 }
