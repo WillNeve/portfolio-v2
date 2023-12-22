0a0df78efa98bc642b669ee564cdf33633083a7a
William Neve <williamneve6000@gmail.com>
2023-12-22 02:57:09 +0000

    cd works and unknown command feedback given

--- src/app/components/terminal/terminal.js
+++ src/app/components/terminal/terminal.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import styles from './terminal.module.scss'
 
@@ -24,7 +24,8 @@ const Input = ({ onSubmit }) => {
     <textarea className={styles.input} onKeyDown={handleKeyDown}
               value={inputValue}
               onChange={(e) => setInputValue(e.target.value)}
-              rows='1'>
+              rows='1'
+              autoCapitalize='none'>
     </textarea>
   )
 }
@@ -41,32 +42,63 @@ const Prompt = ({path}) => {
 
 
 const Terminal = () => {
-  const [lines, setLines] = useState([]);
+  const inputRef = useRef(null)
+  const [lines, setLines] = useState([])
   const [path, setPath] = useState('home')
+  const paths = ['~', 'home', 'test']
+
+  const appendNewLine = (text, response) => {
+    let localLines;
+    if (response) {
+      localLines = lines.concat([[text, path, false], [response, path, true]])
+    } else {
+      localLines = lines.concat([[text, path, false]])
+    }
+    setLines(localLines)
+  }
+
 
   const handleInputSubmit = (text) => {
-    if (/clear *$/.test(text)) {
+    const clearCommand = /clear *$/;
+    const cDCommand = /^\s?cd (\w+\/?)$/;
+
+    if (clearCommand.test(text)) {
       setLines([])
-      console.log(lines);
+    } else if (cDCommand.test(text)) { // cd command
+      const newPath = text.match(cDCommand)[1];
+
+      if (paths.includes(newPath)) {
+        appendNewLine(text)
+        setPath(newPath)
+      } else {
+        const errorMessage = `no such file or directory: ${newPath}`
+        appendNewLine(text, errorMessage)
+      }
     } else {
-      const localLines = lines.concat([[text, path]])
-      setLines(localLines)
+      const errorMessage = `command not found: ${text}`
+      appendNewLine(text, errorMessage)
     }
   }
 
+  const focusInput = () => {
+    // Focus on the textarea when the wrapper is clicked
+    alert('focusing')
+    inputRef.current.focus();
+  };
+
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.wrapper} onClick={focusInput}>
       <div className={styles.linesContainer}>
         {lines.map((line, index) => (
             <div key={index} className={styles.lineWrapper}>
-              <Prompt path={line[1]}/>
+              {line[2] ? ('') : (<Prompt path={line[1]}/>)}
               <Line content={line[0]}/>
             </div>
           ))}
       </div>
       <div className={styles.inputWrapper}>
         <Prompt path={path}/>
-        <Input onSubmit={handleInputSubmit}/>
+        <Input onSubmit={handleInputSubmit} ref={inputRef}/>
       </div>
     </div>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ export default function Home() {
     <main className={styles.main}>
       <CrtScreen/>
       <Terminal/>
+      
     </main>
   )
 }
